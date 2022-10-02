3b0fa6a3b0f6194f29cb0d19d9115dc836d27e7d
Vander Lima <vandercbl@gmail.com>
2022-10-02 17:41:23 -0300

    Criação do componente dashboard e summary contendo a entrada, saída e total

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
 	:root {
 		--background: #f0f2f5;
 		--red: #e52e4d;
+		--green: #33cc95;
 		--blue: #5429cc;
 		--blue-light: #6933ff;
 		--text-title: #363f5f;
@@ -30,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	body {
-		background: var(---background);
+		background: var(--background);
 		-webkit-font-smoothing: antialiased;
 	}
 
