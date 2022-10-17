50cf76ad2d23faab75c56ad030daaefc2afd4e3d
Vander Lima <vandercbl@gmail.com>
2022-10-17 11:18:03 -0300

    Adicionando a lib mirage js para simular e mockar o backend

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+// Utilizando o mirage js para ser um mock do backend
+createServer({
+	routes() {
+		this.namespace = 'api'
+
+		this.get('/transactions', () => {
+			return [
+				{
+					id: 1,
+					title: 'Transaction 1',
+					amount: 400,
+					type: 'deposit',
+					category: 'Food',
+					createdAt: new Date()
+				}
+			]
+		})
+	}
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
