8d04153c86e4679f145dc10e021ff921f1bc8823
Vander Lima <vandercbl@gmail.com>
2022-10-21 17:08:08 -0300

    Adicionando elementos do fomulário e formatação global do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal'
+import {Container} from './styles'
 
 interface NewTransactionModalProps {
 	isOpen: boolean
@@ -10,8 +11,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 		<Modal
 			isOpen={isOpen}
 			onRequestClose={onRequestClose}
+			overlayClassName="react-modal-overlay"
+			className="react-modal-content"
 		>
-			<h2>Cadastrar Transação</h2>
+			<Container>
+				<h2>Cadastrar Transação</h2>
+
+				<input type="text" placeholder='Título'/>
+				<input type="number" placeholder='Valor'/>
+				<input type="text" placeholder='Categoria'/>
+
+				<button type='submit'>
+					Cadastrar
+				</button>
+
+			</Container>
 		</Modal>
 
 	)
