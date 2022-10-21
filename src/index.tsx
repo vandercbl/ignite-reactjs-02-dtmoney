8a9439f7dcd941c3a07d20c62d4a367a57c21e37
Vander Lima <vandercbl@gmail.com>
2022-10-21 18:29:37 -0300

    Criando banco fake usando mirage e fazendo integração para a listagem

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 // Utilizando o mirage js para ser um mock do backend
 createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Website',
+					type: 'deposit',
+					category: 'Dev',
+					amount: 6000,
+					createdAt: new Date('2021-02-12 09:00:00')
+				},
+				{
+					id: 2,
+					title: 'Aluguel',
+					type: 'withdraw',
+					category: 'Casa',
+					amount: 1000,
+					createdAt: new Date('2021-02-13 11:00:00')
+				}
+			]
+		})
+	},
+
 	routes() {
 		this.namespace = 'api'
 
 		this.get('/transactions', () => {
-			return [
-				{
-					id: 1,
-					title: 'Transaction 1',
-					amount: 400,
-					type: 'deposit',
-					category: 'Food',
-					createdAt: new Date()
-				}
-			]
+			return this.schema.all('transaction')
+		})
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody)
+
+			return schema.create('transaction', data)
 		})
 	}
 })
