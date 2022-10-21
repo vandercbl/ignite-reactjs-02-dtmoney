f7e71389be2e13568f7b780f3be32940aba0f71a
Vander Lima <vandercbl@gmail.com>
2022-10-21 17:55:13 -0300

    Formatação de cor condicional utilizando verificação de props com constant

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -35,6 +35,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 						type="button"						
 						onClick={() => setType('deposit')}
 						isActive={type === 'deposit'}
+						activeColor="green"
 					>
 						<img src={incomeImg} alt="Entrada" />
 						<span>Entrada</span>
@@ -43,6 +44,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 						type="button"
 						onClick={() => setType('withdraw')}
 						isActive={type === 'withdraw'}
+						activeColor="red"
 					>
 						<img src={outcomeImg} alt="Saída" />
 						<span>Saída</span>
