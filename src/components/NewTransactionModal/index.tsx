80c795cf3a44f76b525cc88f4f8cdf4459224996
Vander Lima <vandercbl@gmail.com>
2022-10-21 17:46:51 -0300

    Formatação de botão com props no styledComponents e typescript

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
+import {useState} from 'react'
 import Modal from 'react-modal'
-import {Container, TransactionTypeContainer} from './styles'
+import {Container, TransactionTypeContainer, RadioBox} from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+	const [type, setType] = useState('deposit');
+
 	return(
 		<Modal
 			isOpen={isOpen}
@@ -28,18 +31,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 				<input type="number" placeholder='Valor'/>
 
 				<TransactionTypeContainer>
-					<button
-						type="button"
+					<RadioBox
+						type="button"						
+						onClick={() => setType('deposit')}
+						isActive={type === 'deposit'}
 					>
 						<img src={incomeImg} alt="Entrada" />
 						<span>Entrada</span>
-					</button>
-					<button
+					</RadioBox>
+					<RadioBox
 						type="button"
+						onClick={() => setType('withdraw')}
+						isActive={type === 'withdraw'}
 					>
 						<img src={outcomeImg} alt="Saída" />
 						<span>Saída</span>
-					</button>
+					</RadioBox>
 				</TransactionTypeContainer>
 
 				<input type="text" placeholder='Categoria'/>
