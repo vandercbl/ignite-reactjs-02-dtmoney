0b3591ae352835ed03fc4f8564b3e050b5c16361
Vander Lima <vandercbl@gmail.com>
2022-10-21 17:34:14 -0300

    Estilização do form, utilizando lib polished para ter mais opções de formatação de cores no css

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
-import {Container} from './styles'
+import {Container, TransactionTypeContainer} from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
 	isOpen: boolean
@@ -24,6 +26,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
 				<input type="text" placeholder='Título'/>
 				<input type="number" placeholder='Valor'/>
+
+				<TransactionTypeContainer>
+					<button
+						type="button"
+					>
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</button>
+					<button
+						type="button"
+					>
+						<img src={outcomeImg} alt="Saída" />
+						<span>Saída</span>
+					</button>
+				</TransactionTypeContainer>
+
 				<input type="text" placeholder='Categoria'/>
 
 				<button type='submit'>
